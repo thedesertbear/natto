2b3e47c5682dd3584df02b38153a143e235a9d6c
frazazel <sketchysolid@gmail.com>
2022-10-20 11:42:18 -0500

    Copy DD to aftercoreQuest

--- src/tasks/aftercore.ts
+++ src/tasks/aftercore.ts
@@ -25,6 +25,34 @@ export const AftercoreQuest: Quest = {
 			completed: () => get("breakfastCompleted"),
 			do: () => cliExecute("breakfast"),
 		},
+		{	name: "Daily Dungeon",
+			completed: () => get("dailyDungeonDone"),
+		 	prepare: (): void => {
+				if(itemAmount($item`daily dungeon malware`) > 0)
+					putCloset($item`daily dungeon malware`);
+				if(!get("_dailyDungeonMalwareUsed") && itemAmount($item`fat loot token`) < 3)
+					retrieveItem(1, $item`daily dungeon malware`);
+				retrieveItem(1, $item`eleven-foot pole`);
+				retrieveItem(1, $item`pick-o-matic lockpicks`);
+				retrieveItem(1, $item`ring of detect boring doors`);
+			}
+			do: $location`The Daily Dungeon`,
+			outfit: {
+		 		familiar: $familiar`Grey Goose`,
+		 		weapon: (have($item`the Jokester's Gun`) && !get("_firedJokestersGun")) ? $item`the Jokester's Gun` : undefined,
+		 		acc1: (get("_lastDailyDungeonRoom") % 5 === 4) ? $item`ring of detect boring doors` : undefined,
+				modifier: "750 bonus lucky gold ring, 250 bonus Mr. Cheeng's spectacles, 250 bonus mafia thumb ring, 250 bonus carnivorous potted plant, 100 familiar experience",
+			},
+			combat: new CombatStrategy().macro(
+				new Macro()
+				.tryItem($item`daily dungeon malware`)
+				.tryItem($item`porquoise-handled sixgun`)
+				.trySkill($skill`fire the Jokester's Gun`)
+				.attack()
+				.repeat()
+			),
+			limit: { tries: 18 }; //+3 for unaccounted for wanderers, etc.
+		},
 		{	name: "Garbo",
 			completed: () => (myAdventures() === 0 && !canEat()) || stooperDrunk(),
 			do: () => cliExecute("garbo ascend"),
