67d14561962bb5930c696f34fce6c1bf5b8b301d
frazazel <sketchysolid@gmail.com>
2022-11-08 21:56:53 -0600

    v0.4.3
    - fix logic error in bestFam choosing
    - updated list of recommended items/skills/familiars

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { checkPerms, checkReqs } from "./tasks/sim";
 import { permTiers, printPermPlan } from "./tasks/perm";
 import { $class, $item } from "libram";
 
-const version = "0.4.2";
+const version = "0.4.3";
 
 export const args = Args.create(
   "goorbo",
