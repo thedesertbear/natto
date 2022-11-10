e28805b3e5b62e0e26675c7025920c9534317e88
frazazel <sketchysolid@gmail.com>
2022-11-09 21:12:43 -0600

    testing release v0.4.5
    - use NEP free fights for leveling
    - uneffect Beaten Up from June cleaver
    - abort on failure of some combats
    - fix -ml maximizer issue

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { checkPerms, checkReqs } from "./tasks/sim";
 import { permTiers, printPermPlan } from "./tasks/perm";
 import { $class, $item } from "libram";
 
-const version = "0.4.4";
+const version = "0.4.5";
 
 export const args = Args.create(
   "goorbo",
