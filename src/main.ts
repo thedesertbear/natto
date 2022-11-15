2d6ae4b7463dcdee19aee8e3d089a7c9051ddc83
frazazel <sketchysolid@gmail.com>
2022-11-14 23:09:54 -0600

    v0.5.7
    - fix june cleaver limit
    - fix Asdon Martin fueling
    - drink steel margarita after level 5
    - use BACON to purchase malware, if available
    - use combat delevelers against gators and NEP monster, if missing curse of weaksauce
    - support moonsign selection

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { permTiers, printPermPlan } from "./tasks/perm";
 import { $class, $item } from "libram";
 import { toMoonSign } from "./tasks/utils";
 
-const version = "0.4.6";
+const version = "0.4.7";
 
 export const args = Args.create(
   "goorbo",
