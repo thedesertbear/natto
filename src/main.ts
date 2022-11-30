6470eb362b6bc142b97ee4af47f3ad302778b4a3
frazazel <sketchysolid@gmail.com>
2022-11-29 20:13:55 -0600

    v0.4.17
    - bastille support
    - wish or snapper spleen item, depending on cost

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { permTiers, printPermPlan } from "./tasks/perm";
 import { $class, $item } from "libram";
 import { toMoonSign } from "./tasks/utils";
 
-const version = "0.4.16";
+const version = "0.4.17";
 
 export const args = Args.create(
   "goorbo",
