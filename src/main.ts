4aa093d2b663dcbcdcdd0f25f08bbe0df3eb0eb3
frazazel <sketchysolid@gmail.com>
2022-12-05 10:41:34 -0600

    v0.4.21
    - fix for god lobster havers

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { permTiers, printPermPlan } from "./tasks/perm";
 import { $class, $item } from "libram";
 import { toMoonSign } from "./tasks/utils";
 
-const version = "0.4.20";
+const version = "0.4.21";
 
 export const args = Args.create(
   "goorbo",
