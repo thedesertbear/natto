6a38d93c1a84d6f91f1623645dfbfbf9b6108070
frazazel <sketchysolid@gmail.com>
2022-12-07 19:53:39 -0600

    v0.4.23
    - fix for garbo Tasks not completing due to incorrect completion checks
    - lower threshold to buy seal summoning supplies
    - fix dependencies error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { permTiers, printPermPlan } from "./tasks/perm";
 import { $class, $item } from "libram";
 import { toMoonSign } from "./tasks/utils";
 
-const version = "0.4.22";
+const version = "0.4.23";
 
 export const args = Args.create(
   "goorbo",
@@ -89,7 +89,7 @@ export const args = Args.create(
       default: "garbo",
     }),
     garboascend: Args.string({
-      help: `The command that will be used to diet and use all your adventures in Day 2 aftercore.`,
+      help: `The command that will be used to diet and use all your adventures in Day 2 aftercore. Goorbo will append " -100" to this string in some cases, expecting this script to stop when you have 100 adventures remaining, and may call it again.`,
       default: "garbo ascend",
     }),
     voatest: Args.boolean({
