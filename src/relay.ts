529e61d345122112220a64e56329de1aebee2c45
libraryaddict <github@lib.co.nz>
2022-12-10 07:12:27 +1300

    Add some decoration

--- src/relay.ts
+++ src/relay.ts
@@ -47,9 +47,16 @@ function convertArgsToHtml(): RelayPage[] {
     .forEach((p) => {
       const html: ComponentHtml = {
         type: "html",
-        data: `<h1 style="text-align: center;">Goorbo ${p.page}</h1>`,
+        data: `<h1 style="text-align: center;">Goorbo ${p.page}</h1><div class="meat"> <div> <img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"></img></div><div><img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"></img></div><div> <img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px" ></img></div><div><img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"></img></div><div> <img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"></img></div><div> <img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"></div><div><img src="https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"></div>`,
+      };
+      const meat: ComponentHtml = {
+        type: "html",
+        data: `<div class="meat">${`<div style="background-image: https://i.imgur.com/Otj39Jg.png" height="75px" width="75px"/>`.repeat(
+          7
+        )}</div>`,
       };
       p.components.splice(0, 0, html);
+      p.components.push(meat);
     });
 
   return pages.filter((page) => page.components.length > 0);
@@ -58,5 +65,9 @@ function convertArgsToHtml(): RelayPage[] {
 export function main() {
   if (handleApiRequest()) return;
 
-  write(generateHTML(convertArgsToHtml()));
+  write(
+    generateHTML(convertArgsToHtml(), {
+      css: ".meat,.meat div{position:absolute}.meat{width:100%;height:100%;top:0;left:0;overflow:hidden;opacity: 10%;}.meat div{display:block}.meat div:first-child{left:20%;animation:15s linear -2s infinite fall}.meat div:nth-child(2){left:70%;animation:15s linear -4s infinite fall}.meat div:nth-child(3){left:10%;animation:20s linear -7s infinite fall}.meat div:nth-child(4){left:50%;animation:18s linear -5s infinite fall}.meat div:nth-child(5){left:85%;animation:14s linear -5s infinite fall}.meat div:nth-child(6){left:15%;animation:16s linear -10s infinite fall}.meat div:nth-child(7){left:90%;animation:15s linear -4s infinite fall}@keyframes fall{0%{opacity:1;top:-10%;transform:translateX (20px) rotate(0)}20%{opacity:.8;transform:translateX (-20px) rotate(45deg)}40%{transform:translateX (-20px) rotate(90deg)}60%{transform:translateX (-20px) rotate(135deg)}80%{transform:translateX (-20px) rotate(180deg)}100%{top:110%;transform:translateX (-20px) rotate(225deg)}}",
+    })
+  );
 }
