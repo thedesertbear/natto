b70b5cd94fa53fbc8e654e962c4f716782ed5f57
frazazek <sketchysolid@gmail.com>
2023-01-26 06:56:08 -0600

    v0.5.5
    - `list` argument
    - skip clockwork maid if it's too expensive

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { checkPerms, checkReqs } from "./tasks/sim";
 import { args } from "./args";
 import { printPermPlan } from "./tasks/perm";
 
-const version = "0.5.4";
+const version = "0.5.5";
 
 export function main(command?: string): void {
   Args.fill(args, command);
