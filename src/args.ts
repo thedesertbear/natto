4947a671dd4bed4a4ab97fdb230921fc92f41764
frazazel <sketchysolid@gmail.com>
2023-02-07 16:03:31 -0600

    rearrange some args

--- src/args.ts
+++ src/args.ts
@@ -8,19 +8,19 @@ export const args = Args.create(
   "goorbo",
   `Written by frazazel (ign: SketchySolid #422389). This is a full-day script for half-glooping. It aims to be a single-press script that will take you through your Aftercore and Grey You legs, collecting fat loot tokens, getting a Steel Liver, and leveling up to level 13 before running garbo. It chooses a class for you to learn guild skills, and to perm learned skills upon ascension.`,
   {
+    //alternate-run flags
     version: Args.flag({
       help: "Output script version number and exit.",
       default: false,
       setting: "",
     }),
-    actions: Args.number({
-      help: "Maximum number of actions to perform, if given. Can be used to execute just a few steps at a time.",
-    }),
-    abort: Args.string({
-      help: "If given, abort during the prepare() step for the task with matching name.",
-    }),
     sim: Args.flag({
       help: "If set, see the recommended items and skills, then return without taking any actions.",
+      default: false,
+      setting: "",
+    }),
+    simperms: Args.flag({
+      help: "If set, see your current and available perms, as well as the plan for this run, then return without taking any actions.",
       default: false,
       setting: "",
     }),
@@ -29,11 +29,15 @@ export const args = Args.create(
       setting: "",
     }),
 
-    simperms: Args.flag({
-      help: "If set, see your current and available perms, as well as the plan for this run, then return without taking any actions.",
-      default: false,
-      setting: "",
+    //partial run args
+    actions: Args.number({
+      help: "Maximum number of actions to perform, if given. Can be used to execute just a few steps at a time.",
     }),
+    abort: Args.string({
+      help: "If given, abort during the prepare() step for the task with matching name.",
+    }),
+
+    //configuration args
     permtier: Args.number({
       help: `Target perming all skills in the given tier and all better tiers. Choose 0 to only perm non-gnome, non-guild skills that you may have manually learned`,
       options: [[-1, "Do not perm anything"] as [number, (string | undefined)?]].concat(
@@ -45,7 +49,6 @@ export const args = Args.create(
       // options: [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8].map((num) => [num]),
       default: 6,
     }),
-
     pvp: Args.flag({ help: "If true, break hippy stone and do pvp.", default: false }),
     astralpet: Args.custom(
       {
@@ -109,7 +112,6 @@ export const args = Args.create(
       help: `What level to target via adventuring in Uncle Gator's after breaking the prism`,
       default: 13,
     }),
-
     buffy: Args.boolean({
       help: "Set this to false to stop asking Buffy for buffs.",
       default: true,
@@ -141,7 +143,6 @@ export const args = Args.create(
       help: `If set, will run your d2 garbo turns just like normal, but will separately track the last 100 turns, to give you an estimate of what your real-world valueOfAdventure is. Divide your total "VoA Test" profit by 100 for your VoA estimate. Note that it might show > 100 adventures spent, if garbo equipped the mafia thumb ring, June cleaver, or other adventure gaining equipment. This flag may be ignored if a custom setting of garboascend is used`,
       default: true,
     }),
-
     tip: Args.flag({
       help: "Send all your soap knives to the author. Thanks!",
       default: false,
