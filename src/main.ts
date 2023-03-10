626c44b12e9511937879f1fa765de5eb07f67ffc
frazazel <sketchysolid@gmail.com>
2023-03-10 11:44:19 -0600

    v0.6.5
    - switched moonsign default (prev: vole, new: blender)
    - moonsign swapping to wombat post-leveling (drinks 1 pilsner with shotglass to get blender adventures first).
    - fix for goorbo/levelup clan argument mismatch

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { checkPerms, checkReqs } from "./tasks/sim";
 import { args } from "./args";
 import { printPermPlan } from "./tasks/perm";
 
-const version = "0.6.4";
+const version = "0.6.5";
 
 export function main(command?: string): void {
   Args.fill(args, command);
